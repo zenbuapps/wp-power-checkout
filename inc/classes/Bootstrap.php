8f07c8f50f2ddb21f19cad39454eebb28df54fd7
J7 <j7.dev.gg@gmail.com>
2025-09-30 16:12:05 +0800

    fix: 修正 order note css

--- inc/classes/Bootstrap.php
+++ inc/classes/Bootstrap.php
@@ -5,6 +5,7 @@ declare ( strict_types = 1 );
 namespace J7\PowerCheckout;
 
 use J7\PowerCheckout\Utils\Base;
+use J7\WpUtils\Classes\WP;
 use Kucrut\Vite;
 
 if ( class_exists( 'J7\PowerCheckout\Bootstrap' ) ) {
@@ -24,7 +25,8 @@ final class Bootstrap {
 		Domains\Settings\Services\SettingApiService::register_hooks();
 		Domains\Settings\Services\SettingTabService::register_hooks();
 
-		\add_action( 'before_woocommerce_init', [ $this, 'declare_compatibility' ] );
+		\add_action( 'before_woocommerce_init', [ __CLASS__, 'declare_compatibility' ] );
+		\add_action('admin_head', [ __CLASS__, 'custom_css' ]);
 	}
 
 
@@ -33,7 +35,7 @@ final class Bootstrap {
 	 *
 	 * @return void
 	 */
-	public function declare_compatibility(): void {
+	public static function declare_compatibility(): void {
 		if ( !class_exists( '\Automattic\WooCommerce\Utilities\FeaturesUtil' ) ) {
 			return;
 		}
@@ -42,5 +44,17 @@ final class Bootstrap {
 			'cart_checkout_blocks',
 			'power-checkout\plugin.php'
 		);
+	}
+
+	/** @return void 自訂 css */
+	public static function custom_css(): void {
+		?>
+		<style>
+			.order_notes .array_to_html{
+				word-break: break-all;
+				white-space: normal;
+			}
+		</style>
+		<?php
 	}
 }
